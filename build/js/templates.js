77e3c4d5f125bad619546b143a46c28804830254
Damia Dembicki <nikeid1235@gmail.com>
2021-04-06 11:35:35 +0200

    Structure cleanup

--- build/js/templates.js
+++
@@ -1,6 +0,0 @@
-this["MyApp"] = this["MyApp"] || {};
-this["MyApp"]["templates"] = this["MyApp"]["templates"] || {};
-this["MyApp"]["templates"]["home"] = this["MyApp"]["templates"]["home"] || {};
-this["MyApp"]["templates"]["home"]["header"] = Handlebars.template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<h1>Hello templates</h1>";
-},"useData":true});
