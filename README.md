b6ca65b1a6fe4ecc8ae07fb0b09ab542671fda07
Damia Dembicki <nikeid1235@gmail.com>
2021-04-06 15:45:28 +0200

    Added profile pictures, css changes and updated README

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # team-slider
+
+## Tech stack used:
+
+- JS (ES6)
+- GULP
+- SASS
+- SWIPER
+
+## Getting started
+
+Edit team members info in `/data/team.js` file.
+
+```js
+{
+    firstName: 'Basia',
+    lastName: 'Sołtysińska',
+    role: 'Founder',
+    location: 'Warsaw',
+    imageURL: './assets/person1.png',
+    email: '',
+}
+```
+
+You can specify the email, if not the default email will be `firstName.lastName@indahash.com`
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,28 +1,53 @@
 import team from '../data/team.js';
 
+const removeSpecialChars = (str) => {
+  return str
+    .replace(/ą/g, 'a')
+    .replace(/Ą/g, 'A')
+    .replace(/ć/g, 'c')
+    .replace(/Ć/g, 'C')
+    .replace(/ę/g, 'e')
+    .replace(/Ę/g, 'E')
+    .replace(/ł/g, 'l')
+    .replace(/Ł/g, 'L')
+    .replace(/ń/g, 'n')
+    .replace(/Ń/g, 'N')
+    .replace(/ó/g, 'o')
+    .replace(/Ó/g, 'O')
+    .replace(/ś/g, 's')
+    .replace(/Ś/g, 'S')
+    .replace(/ż/g, 'z')
+    .replace(/Ż/g, 'Z')
+    .replace(/ź/g, 'z')
+    .replace(/Ź/g, 'Z');
+};
 
 const renderTeam = () => {
   // selectors
   const teamEl = document.querySelector('.members');
 
-  const toRender = team.map(({ firstName, lastName, role, location, imageURL }) => {
-    const mailAddr = `${firstName.toLowerCase()}.${lastName.toLowerCase()}@indahash.com`;
-    return `
+  const toRender = team
+    .map(({ firstName, lastName, role, location, imageURL, email }) => {
+      const mail = `${removeSpecialChars(firstName).toLowerCase()}.${removeSpecialChars(lastName).toLowerCase()}@indahash.com`;
+      return `
     <li class="member">
-    <a href="mailto:${mailAddr}"><div class="email"><img src="./assets/mail.svg"></div></a>
-    <img
-    src="${imageURL}"
-    alt="avatar"
-    />
-    <h3 class="name">${firstName} ${lastName}</h3>
-    <div class="details">
-    <span class="role">${role}</span>
-    <span class="location">${location}</span>
-    </div>
-    
+      <a href="mailto:${mail}">
+        <div class="email">
+          <img src="./assets/mail.svg">
+        </div>
+      </a>
+      <img
+        src="${imageURL}"
+        alt="avatar"
+      />
+      <h3 class="name">${firstName} ${lastName}</h3>
+      <div class="details">
+        <span class="role">${role}</span>
+        <span class="location">${location}</span>
+      </div>
     </li>
     `;
-  }).join('');
+    }).join('');
 
   // render items
   teamEl.innerHTML = toRender;
